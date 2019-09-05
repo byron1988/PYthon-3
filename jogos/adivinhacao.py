74f2a96c4afb7e0dba3a0442b1d4b846690b5af0
ubirani <you@example.com>
2019-09-05 12:24:58 -0300

    finalizado aula 6

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,15 +1,24 @@
+import random
+
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 31
+numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
 rodada = 1
+print(numero_secreto)
 
-while (rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = input("Digite o seu número: ")
+    chute = input("Digite um número entre 1 e 100:")
     print("você digitou ", chute)
+
+    if (int(chute) < 1 or int(chute) > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
 
     acertou = int(chute) == numero_secreto
     maior = int(chute) > numero_secreto
@@ -17,6 +26,7 @@ while (rodada <= total_de_tentativas):
 
     if(acertou):
         print("você acertou")
+        break
     else:
         if(maior):
             print("Você errou! o  seu chute foi maior do que o número secreto.")
@@ -24,6 +34,4 @@ while (rodada <= total_de_tentativas):
             print("Você errou! o  seu chute foi menor do que o número secreto.")
 
     print("Fim de jogo!")
-
-    rodada = rodada + 1
 
