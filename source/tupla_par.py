844d8214638519abd71b839577b0fc0df7bc5ca7
ubirani <you@example.com>
2020-02-04 00:38:31 -0300

    função recebe tupla e retorna uma nova tupla

---
+++ source/tupla_par.py
@@ -0,0 +1,16 @@
+tupla_1 = (0,1,2,3,4,5,6,7,8)
+def tupla_par(tupla):
+
+    '''
+    tupla: uma tupla
+    
+    retorna: nova tupla formada pelos elementos que possuem índices pares
+    '''
+    lista = []
+    for elemento in tupla:
+        if elemento% 2 == int():
+            lista.append(elemento)
+            nova_tupla = tuple(lista)
+    print(nova_tupla)
+    
+tupla_par(tupla_1)
